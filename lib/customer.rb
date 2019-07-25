897e2c59f0dec82961cfb90ac70c607a52bbd307
Robinson Regalado <rregalado17@gmail.com>
2019-07-25 16:56:44 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,6 +14,10 @@ class Customer
     @@all 
   end
   
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
   def meals
     Meal.all.select do |meal|
       meal.customer == self 
