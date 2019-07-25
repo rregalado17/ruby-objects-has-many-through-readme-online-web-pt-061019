f1bb04b0ca2988a446b5a192fe89363b1dec2dd2
Robinson Regalado <rregalado17@gmail.com>
2019-07-25 19:54:22 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -28,7 +28,7 @@ class Waiter
   
   def best_tipper
     best_tipped_meal = meals.max do |meal_1, meal_2, meal_3, meal_4|
-      meal_1.tip <=> meal_2.tip <=> meal_3 <=> meal_4
+      meal_1.tip <=> meal_2.tip 
     end
      best_tipped_meal.customer
   end
